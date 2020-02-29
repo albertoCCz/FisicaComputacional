5f0977a408692520b88af4337b66b508d332cdc6
albertoCCz <61499152+albertoCCz@users.noreply.github.com>
2020-02-29 22:23:36 +0100

    Added some comments

--- PI/PI.c
+++ PI/PI.c
@@ -5,17 +5,17 @@ int main()
 //calcula pi mediante el metodo Monte Carlo
 {
     double x,y,r0,r,pi; //coordenadas,distanticia al origen, pi
-    FILE *f;
+    FILE *f;    //puntero que apunta al archivo con los puntos generados
     int p = 0;  //puntero donde se guardan el numero de puntos que caen en el circulo
 
     f = fopen("RawPoints.txt","r");
 
-    r0 = 1./4.;
+    r0 = 1./4.; //radio al cuadrado del circulo
 
     while(fscanf(f,"%lf\t%lf",&x,&y)!=EOF)
     //calcula cuantos puntos caen dentro del area del circulo
     {
-        r = pow(x,2) + pow(y,2);
+        r = pow(x,2) + pow(y,2);    //distancia al cuadrado de los puntos
         if(r <= r0)
         {
             p++;
@@ -24,7 +24,8 @@ int main()
 
     fclose(f);
 
-    pi = p*4./25996.;
+    pi = p*4./25996.;   //pi obtenido a partir de relacion de cocientes: 
+                        //(area circulo)/(area cuadrado) = (#puntos en el circulo)/(#puntos generados) 
 
     printf("PI es aproximadamente: %lf\n",pi);
 
