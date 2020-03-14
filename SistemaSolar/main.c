71f1b87d66caea9f38f8d6716f000de656936258
albertoCCz <61499152+albertoCCz@users.noreply.github.com>
2020-03-14 20:18:37 +0100

    Add files via upload

--- SistemaSolar/main.c
+++ SistemaSolar/main.c
@@ -2,80 +2,126 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define N 100 //numero de iteraciones del algoritmo
+double distThirdPow(double **r, int n, int i, int k);
+void aceleracion(double **r, double **a, double **ah, double *m, int n);
+void posicion(double **r, double **v, double **a, double h, int n);
+void velocidad(double **r, double **v, double **a, double **ah, double h, int n);
 
 int main()
 {
-    double h,t,m,n,r[][3],v[][3];
+    int i,iter,n,N = 10000000;
+    double h = 0.01;
     FILE *f1,*f2;
 
-    f1 = fopen("Datos_Iniciales.txt","r");
-    f2 = fopen("Resultados_Dinamica.txt","w");
+    f1 = fopen("Datos_Iniciales.txt", "r");
+    f2 = fopen("Posiciones.txt", "w");
 
-    fscanf(f1,"%lf\n%lf\n%lf%lf\n%lf\n%lf%lf\n%lf",&h,&t,&m,&n,&r,&v);
+    printf("Numero de objetos estelares?\n");
+    scanf("%d",&n);
 
-    aceleracion(&r,&m,n); //aceleracion inicial
-
-    for(i=0;i<N;i++)
+    //creamos los punteros a punteros que contienen la info dinamica
+    double **r = (double **)malloc(n*sizeof(double *));
+    double **v = (double **)malloc(n*sizeof(double *));
+    double **a = (double **)malloc(n*sizeof(double *));
+    double **ah = (double **)malloc(n*sizeof(double *));
+    double *m = (double *)malloc(n*sizeof(double));
+    for(i=0;i<n;i++)
     {
-        posicion(&r, &v, &a, h, n); //calula r(t+h)
-
-
+        *(r+i) = (double *)malloc(2*sizeof(double));
+        *(v+i) = (double *)malloc(2*sizeof(double));
+        *(a+i) = (double *)malloc(2*sizeof(double));
+        *(ah+i) = (double *)malloc(2*sizeof(double));
     }
-
-
-
-    printf("%lf\n",h);
-    return 0;
-}
-
-void aceleracion(double *r[][3], double m, int n)
-{
-    double *a[][3];
-    int i,j,k;
 
     for(i=0;i<n;i++)
     {
-        for(j=0;j<n;j++)
+        fscanf(f1,"%lf\t%lf\t%lf\t%lf\t%lf",*(r+i),(*(r+i)+1),*(v+i),(*(v+i)+1),m+i);
+    }
+
+    //calculamos el valor inicial de las aceleraciones
+    aceleracion(r,a,ah,m,n);
+
+    //implementamos el algoritmo de Verlet con todas las funciones que hemos definido
+    for(iter=0;iter<N;iter++)
+    {
+
+        posicion(r,v,ah,h,n);
+        fprintf(f2,"%lf\t%lf\n",**(r+0),*(*(r+0)+1));
+        aceleracion(r,a,ah,m,n);
+        velocidad(r,v,a,ah,h,n);
+
+    }
+
+    free(r);
+    free(v);
+    free(a);
+    free(ah);
+
+    return 0;
+}
+
+
+double distThirdPow(double **r, int n, int i, int k)
+//calcula la distancia entre los cuerpos i y k y la eleva al cubo
+{
+    double mod,temp = 0;
+    int j;
+
+    if(k!=i)
+    {
+        for(j=0;j<2;j++)
         {
-            if(j!=i)
+            temp = temp + pow((*(*(r+i)+j) - *(*(r+k)+j)),2);
+        }
+    }
+
+    mod = pow(temp,3./2.);
+
+    return mod;
+}
+
+void aceleracion(double **r, double **a, double **ah, double *m, int n)
+{
+    int i,k,j;
+
+    for(i=0;i<n;i++)
+    {
+        for(k=0;k<n;k++)
+        {
+            if(k!=i)
             {
-                for(k=0;k<3;k++)
+                for(j=0;j<2;j++)
                 {
-                    a[i][k] = -m[j]*(r[i][k]-r[j][k])/pow(abs(r[i][k]-r[j][k]),3);
+                    *(*(a+i)+j) = *(*(ah+i)+j); //copiamos la info de a(t+h) en a(t)
+                    *(*(ah+i)+j) = (*(m+k) * (*(*(r+i)+j) - *(*(r+k)+j)))/distThirdPow(r,n,i,k); //actualizamos a(t+h)
                 }
             }
         }
     }
-
-    return;
 }
 
-void posicion(double *r[][3], double *v[][3], double *a[][3], double h, int n)
+void posicion(double **r, double **v, double **a, double h, int n)
 {
     int i,j;
 
     for(i=0;i<n;i++)
     {
-        for(j=0;j<n;j++)
+        for(j=0;j<2;j++)
         {
-            r[i][j] = r[i][j] + h*v[i][j] + pow(h,2)/2.*a[i][j];
+            *(*(r+i)+j) = *(*(r+i)+j) + h * (*(*(v+i)+j)) + pow(h,2)/2. * (*(*(a+i)+j));
         }
     }
 }
 
-void velocidad(double v[][3], double a[][3], double ah[][3], double h, int n)
+void velocidad(double **r, double **v, double **a, double **ah, double h, int n)
 {
     int i,j;
 
     for(i=0;i<n;i++)
     {
-        for(j=0;j<n;j++)
+        for(j=0;j<2;j++)
         {
-            v[i][j] = v[i][j] + h/2.*(a[i][j]+ah[i][j]);
+            *(*(v+i)+j) = *(*(v+i)+j) + h/2. * (*(*(a+i)+j) + *(*(ah+i)+j));
         }
     }
-
-    return;
 }
-
