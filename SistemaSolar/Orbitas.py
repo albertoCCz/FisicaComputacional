591471f4df642ab8013145db2837f1144843fd01
albertoCCz <61499152+albertoCCz@users.noreply.github.com>
2020-03-25 18:42:29 +0100

    Add files via upload

--- SistemaSolar/Orbitas.py
+++ SistemaSolar/Orbitas.py
@@ -1,24 +1,43 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-f = open("Posiciones.txt","r")  #open data file
+f = open("Posiciones.txt","r")
 
-N = 40001   #number of points for each one
-n = 9       #number of objects (8 planets + the Sun)
+N = 20001
+n = 9
 x = np.zeros((n,N))
 y = np.zeros((n,N))
+space = np.linspace(1,N,N)
+eCin = []
+ePot = []
+eTot = []
 
-#read the file and save the data
 for i in range(N):
     lineList = f.readline().split()
+    eCin.append(float(lineList[-2]))
+    ePot.append(float(lineList[-1]))
+    eTot.append(float(eCin[i])+float(ePot[i]))
+
     for j in range(n):
         x[j][i], y[j][i] = lineList[2*j], lineList[2*j+1]
 
-#plot the orbits
+"""
 for i in range(n):
     plt.plot(x[i],y[i])
 plt.xlabel("r_x (UA)")
 plt.ylabel("r_y (UA)")
-plt.title("Órbitas planetas Sistema Solar")
+plt.legend(["Sol", "Mercurio", "Venus", "Tierra",
+            "Marte", "Júpiter", "Saturno", "Urano", "Neptuno"])
+plt.title("Órbitas planetas relativas a la Tierra")
 plt.grid()
+"""
+plt.grid()
+plt.title("Conservación Energía Sistema Solar")
+plt.plot(space,eCin)
+plt.plot(space,ePot)
+plt.plot(space,eTot)
+plt.legend(["Cinética","Potencial","Total"])
+plt.ylabel("Energía")
+plt.xlabel("Iteración/1000")
+
 plt.show()
--- SistemaSolar/main.c
+++ SistemaSolar/main.c
@@ -2,15 +2,18 @@
 #include <stdlib.h>
 #include <math.h>
 
-double distThirdPow(double **r, int i, int k);
 void aceleracion(double **r, double **ah, double *m, int n);
-void copyVector(double **v1, double **v2, int n);
 void posicion(double **r, double **v, double **a, double h, int n);
 void velocidad(double **r, double **v, double **a, double **ah, double h, int n);
+void copyVector(double **v1, double **v2, int n);
+double distancia(double **r, int i, int k);
+double moduloCuadrado(double **v1, int i);
+double eCinetica(double **v, double *m, int n);
+double ePotencial(double **r, double *m, int n);
 
 int main()
 {
-    int i,iter,n,N = 1000001;
+    int i,j,iter,n,N = 20000001;
     double h = 0.0001;
     FILE *f1,*f2;
 
@@ -39,6 +42,21 @@ int main()
         fscanf(f1,"%lf\t%lf\t%lf\t%lf\t%lf",r[i],r[i]+1,v[i],v[i]+1,&m[i]);
     }
 
+    //aumentamos un 15% la velocidad inicial de los planetas
+    /*for(i=0;i<n;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            r[i][j] = r[i][j]-r[3][j];
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            v[i][j] = v[i][j]-v[3][j];
+        }
+    }*/
     //calculamos el valor inicial de las aceleraciones
     aceleracion(r,ah,m,n);
 
@@ -49,21 +67,39 @@ int main()
         copyVector(a,ah,n);
         aceleracion(r,ah,m,n);
         velocidad(r,v,a,ah,h,n);
+        /*for(i=0;i<n;i++)
+        {
+            for(j=0;j<2;j++)
+            {
+                r[i][j] = r[i][j]-r[3][j];
+            }
+        }
 
-        //guardamos en un fichero las sucesivas posiciones de los cuerpos
         for(i=0;i<n;i++)
         {
-                fprintf(f2,"%lf\t%lf",r[i][0],r[i][1]);   //escribimos en un fichero las sucesivas posiciones de la Tierra
+            for(j=0;j<2;j++)
+            {
+                v[i][j] = v[i][j]-v[3][j];
+            }
+        }*/
+        //guardamos en un fichero las sucesivas posiciones de los cuerpos
+        if(iter%1000 == 0)
+        {
+            for(i=0;i<n;i++)
+            {
+                fprintf(f2,"%lf\t%lf",r[i][0]-r[3][0],r[i][1]-r[3][1]);   //escribimos en un fichero las sucesivas posiciones de los planetas
+
                 if(i==n-1)
                 {
-                    fprintf(f2,"\n");
+                    fprintf(f2,"\t%lf\t%lf\n",eCinetica(v,m,n),ePotencial(r,m,n));
                 }
                 else
                 {
                     fprintf(f2,"\t");
                 }
-        }
 
+            }
+        }
     }
 
     //liberamos el espacio en memoria
@@ -76,8 +112,8 @@ int main()
 }
 
 
-double distThirdPow(double **r, int i, int k)
-//calcula la distancia entre los cuerpos i y k y la eleva al cubo
+double distancia(double **r, int i, int k)
+//calcula la distancia entre los cuerpos i y k
 {
     double mod,temp = 0;
     int j;
@@ -117,7 +153,7 @@ void aceleracion(double **r, double **ah, double *m, int n)
             {
                 if(k!=i)
                 {
-                    ah[i][j] = ah[i][j] - m[k] * (r[i][j] - r[k][j])/pow(distThirdPow(r,i,k),3); //actualizamos a(t+h)
+                    ah[i][j] = ah[i][j] - m[k] * (r[i][j] - r[k][j])/pow(distancia(r,i,k),3); //actualizamos a(t+h)
                 }
                 else
                 {
@@ -169,4 +205,52 @@ void velocidad(double **r, double **v, double **a, double **ah, double h, int n)
             v[i][j] = v[i][j] + h/2. * (a[i][j] + ah[i][j]);
         }
     }
+}
+
+double moduloCuadrado(double **v1, int k)
+{
+    int i;
+    double value = 0.;
+
+    for(i=0;i<2;i++)
+    {
+        value = value + pow(v1[k][i],2);
+    }
+
+    return value;
+}
+
+
+double eCinetica(double **v, double *m, int n)
+{
+    int i,j;
+    double temp = 0.0, eCin = 0.0;
+
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            temp = temp + pow(v[i][j],2);
+        }
+        eCin = eCin + 1./2*m[i]*temp;
+    }
+
+    return eCin;
+}
+
+
+double ePotencial(double **r, double *m, int n)
+{
+    int i,j;
+    double ePot = 0.0;
+
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            ePot = ePot - m[i]*m[j]/distancia(r, i, j);
+        }
+    }
+
+    return ePot;
 }
